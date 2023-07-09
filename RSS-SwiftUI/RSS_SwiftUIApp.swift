453e68a6a8557f6a3d85c4f03b91f74fd872e75d
Jon Podner <jonpodner@jonpodner.com>
2023-07-09 13:44:35 -0500

    Initial Commit

---
+++ RSS-SwiftUI/RSS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_SwiftUIApp.swift
+//  RSS-SwiftUI
+//
+//  Created by Jon Podner on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RSS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
